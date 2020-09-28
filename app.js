ff90e5e6df58a353416ee9807deaf9642b63597d
Tanishq <tanishq2.0.5@outlook.com>
2020-09-28 20:51:51 +0530

    Added Login Page

--- app.js
+++ app.js
@@ -5,9 +5,31 @@ const ejs = require('ejs');
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) { 
+app.route('/').get(function (req, res) { 
+    // Get Method for Home
     res.render('home');
- })
+ }).post(function (req, res) { 
+     // Post Method for Home
+  });
+
+app.route('/contact').get(function (req, res) { 
+    // Get Method Function for Contact
+ }).post(function (req, res) { 
+     // Post Method Function for contact
+  });
+
+app.route('/login').get(function (req, res) {
+    // Get Method for Login
+    res.render('login')
+}).post(function (req, res) { 
+    // Post Method for Login
+ });
+
+app.route('/register').get(function (req, res) { 
+    // Get Method for register
+ }).post(function (req, res) { 
+     // Post Method for register
+  });
 
 const port = 3000;
-app.listen(port, () => console.log(`Example app listening on port port!`));
+app.listen(port, () => console.log("Example app listening on port!"));
